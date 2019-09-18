f80cfdfa001745244db11e245a9f22f8a6ad82bd
Aneto7 <adm.antonioneto@hotmail.com>
2019-09-18 10:35:54 +0100

    Testando primeira classe de dominio

---
+++ src/main/java/com/example/start/domain/Categoria.java
@@ -0,0 +1,54 @@
+package com.example.start.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        if (id != null ? !id.equals(categoria.id) : categoria.id != null) return false;
+        return nome != null ? nome.equals(categoria.nome) : categoria.nome == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (nome != null ? nome.hashCode() : 0);
+        return result;
+    }
+}
--- src/main/java/com/example/start/resources/CategoriaResourse.java
+++ src/main/java/com/example/start/resources/CategoriaResourse.java
@@ -1,16 +1,28 @@
 package com.example.start.resources;
 
+import com.example.start.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResourse {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
