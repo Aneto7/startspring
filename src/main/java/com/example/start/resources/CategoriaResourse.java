c1b927c634c726c2a931fc4435163f6702360c73
Aneto7 <adm.antonioneto@hotmail.com>
2019-09-19 10:36:29 +0100

    Criando repositoory e service para categoria

--- src/main/java/com/example/start/resources/CategoriaResourse.java
+++ src/main/java/com/example/start/resources/CategoriaResourse.java
@@ -2,17 +2,12 @@ package com.example.start.resources;
 
 import com.example.start.domain.Categoria;
 import com.example.start.services.CategoriaService;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
