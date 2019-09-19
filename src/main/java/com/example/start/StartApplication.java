2856f034884a58bb644ba634afa3a0f35600da14
Aneto7 <adm.antonioneto@hotmail.com>
2019-09-19 10:44:51 +0100

    Criando operação de instaciação

--- src/main/java/com/example/start/StartApplication.java
+++ src/main/java/com/example/start/StartApplication.java
@@ -1,13 +1,29 @@
 package com.example.start;
 
+import com.example.start.domain.Categoria;
+import com.example.start.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class StartApplication {
+public class StartApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
