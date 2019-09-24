da71c9e4187b0b58b2161294cf680790ffb9d69e
Aneto7 <adm.antonioneto@hotmail.com>
2019-09-24 23:28:55 +0100

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/example/start/domain/Categoria.java
+++ src/main/java/com/example/start/domain/Categoria.java
@@ -1,5 +1,7 @@
 package com.example.start.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ public class Categoria implements Serializable {
     private Integer id;
     private String nome;
 
+    @JsonManagedReference
     @ManyToMany(mappedBy = "categorias")
     private List<Produto> produtos = new ArrayList<>();
 
