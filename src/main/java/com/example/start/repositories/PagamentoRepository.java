097357313d88a3a29a959df13502bc07d9faa370
Aneto7 <adm.antonioneto@hotmail.com>
2019-09-30 22:19:05 +0100

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/example/start/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.example.start.repositories;
+
+import com.example.start.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
---
+++ src/main/java/com/example/start/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.example.start.repositories;
+
+import com.example.start.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+}
