f00794ac712bf6eb518abce6a79adb1c13df518a
Aneto7 <adm.antonioneto@hotmail.com>
2019-10-08 20:58:55 +0100

    Atualizando com PUT e outras pequenas atualizações

--- src/main/java/com/example/start/resources/CategoriaResourse.java
+++ src/main/java/com/example/start/resources/CategoriaResourse.java
@@ -17,8 +17,8 @@ public class CategoriaResourse {
     private CategoriaService service;
 
     @RequestMapping(value ="/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id){
-        Categoria obj = service.buscar(id);
+    public ResponseEntity<Categoria> find(@PathVariable Integer id){
+        Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
 
@@ -27,6 +27,13 @@ public class CategoriaResourse {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity <Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
     }
 
 }
