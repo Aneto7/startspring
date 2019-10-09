108c220895eccc8eee95b17c410c412471d0a6a3
Aneto7 <adm.antonioneto@hotmail.com>
2019-10-09 20:20:42 +0100

    End point com Page

--- src/main/java/com/example/start/services/CategoriaService.java
+++ src/main/java/com/example/start/services/CategoriaService.java
@@ -6,6 +6,9 @@ import com.example.start.services.exeptions.DataIntegrityException;
 import com.example.start.services.exeptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,5 +43,9 @@ public class CategoriaService {
     }
     public List<Categoria> findAll(){
         return repo.findAll();
+    }
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 }
